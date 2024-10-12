a27cd9306dbc1eb40dd28875260f91226a4b2cc4
Ali07117 <alirashid07117@gmail.com>
2024-10-13 02:31:24 +0500

    update

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -5,8 +5,8 @@ import Navbar from '../components/navbar';
 function Contactpage() {
   return (
     <>
-      <div className="contact-page-wrapper bg-[black] pt-[8rem] sm:pt-[0rem] ">
-        <div className="contact-page-nav-container pt-[6rem] px-[1.5rem]">
+      <div className="contact-page-wrapper bg-[black] pt-[7.3rem] md:pt-[7.3rem] ">
+        <div className="contact-page-nav-container pt-[6rem] md:pt-[3.5rem] lg:pt-[0rem] px-[1.5rem]">
           <Navbar/>
         </div>
         <Contactsection />
