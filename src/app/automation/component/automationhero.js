76b26190c0620facc35cd04ab598fdbe86a34e16
Ali07117 <alirashid07117@gmail.com>
2024-10-20 01:55:22 +0500

    responsive and anmation

--- src/app/automation/component/automationhero.js
+++ src/app/automation/component/automationhero.js
@@ -5,14 +5,14 @@ function AutomationHero() {
     return (
         <>
             <div className="automation-hero-section">
-                <h1 className=' px-[1.5rem] hero-heading text-[white] text-[8rem] leading-[6.5rem] text-[--textprimary] font-average'>Automation Anywhere </h1>
+                <h1 className=' px-[1.5rem] text-[white] hero-heading text-[7.6vw] leading-[6.5rem] text-[--textprimary] font-average'>Automation Anywhere </h1>
                 <Projectintro />
-                <div className='flex gap-[15px] px-[1.5rem] mb-[1rem]'>
+                <div className='flex gap-[15px] px-[1.5rem] mb-[1rem] automation-all-work-box'>
                     <img className='rotate-[180deg]' src="/images/sectionarrow.svg" alt="" />
-                    <p className='font-inter font-[400] text-[18px] text-[--textprimary] text-start'>All Works</p>
+                    <p className='automation-all-work  font-inter font-[400] text-[18px] text-[--textprimary] text-start'>All Works</p>
                 </div>
                 <div className='flex justify-center'>
-                    <div className={` automation-hero-image rounded-[1.5rem] h-[95vh] w-[97%]`} style={{ backgroundImage: `url(/images/section1img.svg)`, backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: "cover" }}></div>
+                    <div className={` automation-hero-image rounded-[1.5rem] h-[95vh] mx-[1.5rem] w-[100%]`} style={{ backgroundImage: `url(/images/section1img.svg)`, backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: "cover" }}></div>
                 </div>
             </div>
         </>
--- src/app/components/otherworksection.js
+++ src/app/components/otherworksection.js
@@ -17,11 +17,11 @@ function OtherWorkSection() {
           <Aboutworkcards company={"Afious"} device={"Mobile App"} beforpara={"Strategy, Experience Design, Visual Design"} />
         </div>
         </div>
-        <div className="other-work-top h-[105px] w-[100%] text-[#00259C] sticky bottom-[0] bg-[#D5D9E4] flex items-center justify-center gap-[1rem]">
-            <p className=' font-average text-[3rem]'>Ready to bring your project to life?</p>
-            <button className='bg-[#00259C] text-[18px] text-[white] font-[400] border-[2px] border-[#044AFF] px-[3rem] py-[13px] rounded-[2rem]'>Contact Us</button>
+        <div className="other-work-top min-h-[105px] w-[100%] text-[#00259C] sticky bottom-[0] bg-[#D5D9E4] flex items-center justify-center gap-[1rem]">
+            <p className=' font-average other-work-top-text text-[3rem]'>Ready to bring your project to life?</p>
+            <button className='bg-[#00259C] text-[18px] other-work-top-button text-[white] font-[400] border-[2px] border-[#044AFF] px-[3rem] py-[13px] rounded-[2rem]'>Contact Us</button>
         </div>
-        <div className='px-[1.5rem] pt-[6rem]'>
+        <div className='px-[1.5rem] automation-footer-nav-container pt-[6rem]'>
     <Footernav/>
         </div>
     </div>
