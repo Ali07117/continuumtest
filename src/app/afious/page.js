da4972a2a9e9c275127a8ed5055218866fcccf55
Ali07117 <alirashid07117@gmail.com>
2024-12-03 18:20:43 +0500

    update

--- src/app/afious/page.js
+++ src/app/afious/page.js
@@ -7,11 +7,14 @@ import AutomationHero from '../automation/component/automationhero';
 import WorkChallenge from '../components/workchallenge';
 import SolutionSection from '../components/solutionsection';
 import OutcomeSection from '../components/outcomesection';
+import Link from 'next/link';
 import AfiousChatSection from '../components/afiouschatsection';
 import OtherWorkSection from '../components/otherworksection';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from "gsap/all";
+import Footernav from '../components/homesections/footernav';
+import Aboutworkcards from '../about/aboutworksection/comp/aboutworkcards';
 gsap.registerPlugin(ScrollTrigger)
 // import LocomotiveScroll from 'locomotive-scroll';
 
@@ -33,38 +36,38 @@ function Afious() {
       }
       
     )()
-  
 
-    gsap.to('.other-work-top-2', {
-      backgroundColor: 'black',
-      color: 'white',
-      scrollTrigger: {
-        trigger: '.other-work-top-2',
-        start: 'top+=200 bottom', // Start 200px into the element
-        end: 'bottom+=200 bottom', // End 200px past the element
-        scrub: true, // Smooth scrubbing
-      },
-    });
+    let showImg1Box = document.querySelector(".show-img-1-box");
+    let showImg2Box = document.querySelector(".show-img-2-box");
+    let showImg1 = document.querySelector(".show-img-1");
+    let showImg2 = document.querySelector(".show-img-2");
+    let otherWorkTop = document.querySelector(".other-work-top");
+    let otherWorkSection = document.querySelector(".other-work-section");
+
+
+  
+    showImg1Box.addEventListener("mouseenter",function(){
+      showImg1.style.opacity="1";
+    })
+    showImg1Box.addEventListener("mouseleave",function(){
+      showImg1.style.opacity="0";
+    })
+  
+    showImg2Box.addEventListener("mouseenter",function(){
+      showImg2.style.opacity="1";
+    })
+    showImg2Box.addEventListener("mouseleave",function(){
+      showImg2.style.opacity="0";
+    })
+  
 
   }, []);
   
   useGSAP(
     () => {
 
+      
 
-      // gsap.to('.other-work-top-2', {
-      //   backgroundColor: 'black',
-      //   color: 'white',
-      //   scrollTrigger: {
-      //     trigger: '.other-work-top-2',
-      //     start: 'top+=200 bottom', // Start 200px into the element
-      //     end: 'bottom+=200 bottom', // End 200px past the element
-      //     scrub: true, // Smooth scrubbing
-      //     markers: false,
-      //   },
-      // });
-
-      // ===================
       gsap.from(".afious-animation-1", {
         y: "30px", // Start the element 100px down from its original position
         opacity:0,
@@ -250,6 +253,19 @@ function Afious() {
           end: "top -70%",
         }
       });
+
+      gsap.to('.other-work-top-2', {
+        opacity: 0,
+        display: 'none',
+        scrollTrigger: {
+          trigger: '.other-work-wrapper-2',
+          start: 'top 100%',
+          end: 'top 50%',
+          scrub: true,
+          markers: true,
+        },
+      }); 
+
     });
   return (
     <>
@@ -310,7 +326,40 @@ function Afious() {
               ></video>
             </div>
           </div>
-          <OtherWorkSection classtops={"other-work-2"} classtop={"other-work-top-2"} />
+          {/* <OtherWorkSection classtops={"other-work-2"} classtop={"other-work-top-2"} /> */}
+          <div className="other-work-wrapper-2 bg-[--bgprimary] relative mt-[12rem] ">
+    <div className="h-[12rem] bg-[white]"></div>
+        <div className="other-work-upper"></div>
+        <div className="other-work-section bg-[--bgprimary] pt-[5rem]">
+        <p className='font-average other-work-section-heading automation-animation-19 afious-animation-13 energy-animation-12 text-[--textprimary] text-[3rem] px-[1.5rem]'>Other Work</p>
+        <div className=" other-work-card-component-wrapper pt-[6rem] pb-[12rem] px-[1.5rem]">
+        <Link href="/automation">
+        <div className="show-img-1-box relative automation-animation-20 afious-animation-14 energy-animation-13">
+        <img className=" opacity-0 show-img-1 duration-300 h-[200px] w-[280px] absolute top-[0px] left-[15%] rounded-[8px] object-cover" src="/images/workimg2.svg" alt="" />
+          <Aboutworkcards pr={"25px"} border={"1px"} company={"Dynegy"} device={"Website"} beforpara={"Visual Design, Experience Design"}  />
+        </div>
+        </Link>
+        <Link href="/afious">
+        <div className="show-img-2-box relative automation-animation-21 afious-animation-15 energy-animation-14">
+        <img className=" opacity-0 show-img-2 duration-300 h-[200px] w-[280px] absolute top-[-100%] left-[15%] rounded-[8px] object-cover" src="/images/workimg3.svg" alt="" />
+          <Aboutworkcards company={"Afious"} device={"Mobile App"} beforpara={"Strategy, Experience Design, Visual Design"} />
+        </div>
+        </Link>
+        </div>
+        </div>
+
+        <div className={` other-work-top min-h-[105px] z-[9] w-[100%] text-[white] sticky bottom-[0] flex items-center justify-center gap-[1rem] bg-[black] `}>
+            <p className=' font-average other-work-top-text text-[3rem]'>Ready to bring your project to life?</p>
+            <Link href="/contact"><button className='bg-[#00259C] text-[18px] other-work-top-button text-[white] font-[400] border-[2px] border-[#044AFF] px-[3rem] py-[13px] rounded-[2rem]'>Contact Us</button></Link>
+        </div>
+        <div className={` other-work-top other-work-top-2 z-[999] min-h-[105px] w-[100%] text-[#00259C] sticky bottom-[0] flex items-center bg-[#D5D9E4] justify-center gap-[1rem]`}>
+            <p className=' font-average other-work-top-text text-[3rem]'>Ready to bring your project to life?</p>
+            <Link href="/contact"><button className='bg-[#00259C] text-[18px] other-work-top-button text-[white] font-[400] border-[2px] border-[#044AFF] px-[3rem] py-[13px] rounded-[2rem]'>Contact Us</button></Link>
+        </div>
+        <div className='px-[1.5rem] automation-footer-nav-container pt-[6rem]'>
+    <Footernav/>
+        </div>
+    </div>
         </div>
       </div>
     </>
--- src/app/components/otherworksection.js
+++ src/app/components/otherworksection.js
@@ -1,118 +1,96 @@
-import React, { useEffect }  from "react";
-import Aboutworkcards from '../about/aboutworksection/comp/aboutworkcards';
-import Footernav from './homesections/footernav';
-import Link from 'next/link';
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-import { ScrollTrigger } from "gsap/all";
-gsap.registerPlugin(ScrollTrigger)
+// import React, { useEffect }  from "react";
+// import Aboutworkcards from '../about/aboutworksection/comp/aboutworkcards';
+// import Footernav from './homesections/footernav';
+// import Link from 'next/link';
+// import gsap from 'gsap';
+// import { useGSAP } from '@gsap/react';
+// import { ScrollTrigger } from "gsap/all";
+// gsap.registerPlugin(ScrollTrigger)
 
-function OtherWorkSection(props) {
-
-  // useEffect(() => {
-  //   const animation = gsap.to('.other-work-top', {
-  //     opacity: 0,
-  //     display: 'none',
-  //     scrollTrigger: {
-  //       trigger: '.other-work-upper',
-  //       start: 'top 60%',
-  //       end: 'bottom 40%',
-  //       scrub: true,
-  //       markers: true,
-  //     },
-  //   });
+// function OtherWorkSection(props) {
   
-  //   ScrollTrigger.refresh(); // Recalculate positions
+//    useGSAP(() => {
+//     gsap.to('.other-work-top-1', {
+//       opacity: 0,
+//       display: 'none',
+//       scrollTrigger: {
+//         trigger: '.other-work-wrapper',
+//         start: 'top 0vh',
+//         end: 'top -40vh',
+//         scrub: true,
+//         markers: true,
+//       },
+//     }); 
+// });
+
+// useEffect(() => { 
   
-  //   return () => {
-  //     animation.scrollTrigger?.kill(); // Cleanup on unmount
-  //   };
-  // }, []);
-
-   useGSAP(() => {
-      gsap.to('.other-work-top', {
-      opacity: 0,
-      display: 'none',
-      scrollTrigger: {
-        trigger: '.other-work-top',
-        start: 'top 60%',
-        end: 'bottom 40%',
-        scrub: true,
-        // markers: true,
-      },
-    });
-
-    // ScrollTrigger.refresh();
-  // }); // Delay initialization slightly
-});
-
-useEffect(() => { 
-  
-  let showImg1Box = document.querySelector(".show-img-1-box");
-    let showImg2Box = document.querySelector(".show-img-2-box");
-    let showImg1 = document.querySelector(".show-img-1");
-    let showImg2 = document.querySelector(".show-img-2");
-    let otherWorkTop = document.querySelector(".other-work-top");
-    let otherWorkSection = document.querySelector(".other-work-section");
+//   let showImg1Box = document.querySelector(".show-img-1-box");
+//     let showImg2Box = document.querySelector(".show-img-2-box");
+//     let showImg1 = document.querySelector(".show-img-1");
+//     let showImg2 = document.querySelector(".show-img-2");
+//     let otherWorkTop = document.querySelector(".other-work-top");
+//     let otherWorkSection = document.querySelector(".other-work-section");
 
 
   
-    showImg1Box.addEventListener("mouseenter",function(){
-      showImg1.style.opacity="1";
-    })
-    showImg1Box.addEventListener("mouseleave",function(){
-      showImg1.style.opacity="0";
-    })
+//     showImg1Box.addEventListener("mouseenter",function(){
+//       showImg1.style.opacity="1";
+//     })
+//     showImg1Box.addEventListener("mouseleave",function(){
+//       showImg1.style.opacity="0";
+//     })
   
-    showImg2Box.addEventListener("mouseenter",function(){
-      showImg2.style.opacity="1";
-    })
-    showImg2Box.addEventListener("mouseleave",function(){
-      showImg2.style.opacity="0";
-    })
+//     showImg2Box.addEventListener("mouseenter",function(){
+//       showImg2.style.opacity="1";
+//     })
+//     showImg2Box.addEventListener("mouseleave",function(){
+//       showImg2.style.opacity="0";
+//     })
 
      
-    }, []);
+//     }, []);
 
  
-  return (
-    <>
-    <div className="other-work-wrapper sticky top-[0px] bg-[--bgprimary] relative mt-[12rem] ">
-    <div className="h-[12rem] bg-[white]"></div>
-        <div className="other-work-upper"></div>
-        <div className="other-work-section bg-[--bgprimary] pt-[5rem]">
-        <p className='font-average other-work-section-heading automation-animation-19 afious-animation-13 energy-animation-12 text-[--textprimary] text-[3rem] px-[1.5rem]'>Other Work</p>
-        <div className=" other-work-card-component-wrapper pt-[6rem] pb-[12rem] px-[1.5rem]">
-        <Link href="/automation">
-        <div className="show-img-1-box relative automation-animation-20 afious-animation-14 energy-animation-13">
-        <img className=" opacity-0 show-img-1 duration-300 h-[200px] w-[280px] absolute top-[0px] left-[15%] rounded-[8px] object-cover" src="/images/workimg2.svg" alt="" />
-          <Aboutworkcards pr={"25px"} border={"1px"} company={"Dynegy"} device={"Website"} beforpara={"Visual Design, Experience Design"}  />
-        </div>
-        </Link>
-        <Link href="/afious">
-        <div className="show-img-2-box relative automation-animation-21 afious-animation-15 energy-animation-14">
-        <img className=" opacity-0 show-img-2 duration-300 h-[200px] w-[280px] absolute top-[-100%] left-[15%] rounded-[8px] object-cover" src="/images/workimg3.svg" alt="" />
-          <Aboutworkcards company={"Afious"} device={"Mobile App"} beforpara={"Strategy, Experience Design, Visual Design"} />
-        </div>
-        </Link>
-        </div>
-        </div>
-        {/* <div className="other-work-upper text-[red]">ss</div>   */}
-        <div className={` ${props.classtops} min-h-[105px] z-[9] w-[100%] text-[white] sticky bottom-[0] flex items-center justify-center gap-[1rem]`} style={{backgroundColor:"black"}}>
-            <p className=' font-average other-work-top-text text-[3rem]'>Ready to bring your project to life?</p>
-            <Link href="/contact"><button className='bg-[#00259C] text-[18px] other-work-top-button text-[white] font-[400] border-[2px] border-[#044AFF] px-[3rem] py-[13px] rounded-[2rem]'>Contact Us</button></Link>
-        </div>
-        <div className={` other-work-top z-[999] min-h-[105px] w-[100%] text-[#00259C] sticky bottom-[0] flex items-center justify-center gap-[1rem]`} style={{backgroundColor:"#D5D9E4"}}>
-            <p className=' font-average other-work-top-text text-[3rem]'>Ready to bring your project to life?</p>
-            <Link href="/contact"><button className='bg-[#00259C] text-[18px] other-work-top-button text-[white] font-[400] border-[2px] border-[#044AFF] px-[3rem] py-[13px] rounded-[2rem]'>Contact Us</button></Link>
-        </div>
-        <div className='px-[1.5rem] automation-footer-nav-container pt-[6rem]'>
-    <Footernav/>
-        </div>
-    </div>
-    
-    </>
-  );
-}
+//   return (
+//     <>
+//     <div className="other-work-wrapper bg-[--bgprimary] relative mt-[12rem] ">
+//     <div className="h-[12rem] bg-[white]"></div>
+//     <p className="text-[red] topp">sss</p>
+//         <div className="other-work-upper"></div>
+//         <div className="other-work-section bg-[--bgprimary] pt-[5rem]">
+//         <p className='font-average other-work-section-heading automation-animation-19 afious-animation-13 energy-animation-12 text-[--textprimary] text-[3rem] px-[1.5rem]'>Other Work</p>
+//         <div className=" other-work-card-component-wrapper pt-[6rem] pb-[12rem] px-[1.5rem]">
+//         <Link href="/automation">
+//         <div className="show-img-1-box relative automation-animation-20 afious-animation-14 energy-animation-13">
+//         <img className=" opacity-0 show-img-1 duration-300 h-[200px] w-[280px] absolute top-[0px] left-[15%] rounded-[8px] object-cover" src="/images/workimg2.svg" alt="" />
+//           <Aboutworkcards pr={"25px"} border={"1px"} company={"Dynegy"} device={"Website"} beforpara={"Visual Design, Experience Design"}  />
+//         </div>
+//         </Link>
+//         <Link href="/afious">
+//         <div className="show-img-2-box relative automation-animation-21 afious-animation-15 energy-animation-14">
+//         <img className=" opacity-0 show-img-2 duration-300 h-[200px] w-[280px] absolute top-[-100%] left-[15%] rounded-[8px] object-cover" src="/images/workimg3.svg" alt="" />
+//           <Aboutworkcards company={"Afious"} device={"Mobile App"} beforpara={"Strategy, Experience Design, Visual Design"} />
+//         </div>
+//         </Link>
+//         </div>
+//         </div>
 
-export default OtherWorkSection;
+//         <div className={` other-work-top min-h-[105px] z-[9] w-[100%] text-[white] sticky bottom-[0] flex items-center justify-center gap-[1rem] bg-[black] `}>
+//             <p className=' font-average other-work-top-text text-[3rem]'>Ready to bring your project to life?</p>
+//             <Link href="/contact"><button className='bg-[#00259C] text-[18px] other-work-top-button text-[white] font-[400] border-[2px] border-[#044AFF] px-[3rem] py-[13px] rounded-[2rem]'>Contact Us</button></Link>
+//         </div>
+//         <div className={` other-work-top other-work-top-1 z-[999] min-h-[105px] w-[100%] text-[#00259C] sticky bottom-[0] flex items-center bg-[#D5D9E4] justify-center gap-[1rem]`}>
+//             <p className=' font-average other-work-top-text text-[3rem]'>Ready to bring your project to life?</p>
+//             <Link href="/contact"><button className='bg-[#00259C] text-[18px] other-work-top-button text-[white] font-[400] border-[2px] border-[#044AFF] px-[3rem] py-[13px] rounded-[2rem]'>Contact Us</button></Link>
+//         </div>
+//         <div className='px-[1.5rem] automation-footer-nav-container pt-[6rem]'>
+//     <Footernav/>
+//         </div>
+//     </div>
+    
+//     </>
+//   );
+// }
+
+// export default OtherWorkSection;
